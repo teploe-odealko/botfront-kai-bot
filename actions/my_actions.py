856ed1f9b563686c048e949ea84a919e7bda962c
dinlat <din.latypov@gmail.com>
2023-04-26 23:20:51 +0300

    one func

--- actions/my_actions.py
+++ actions/my_actions.py
@@ -70,65 +70,65 @@ class FetchStatus(Action):
 
         dispatcher.utter_message(template="utter_to_human")
 
-        print("create_topic")
-        try:
-            bot = telegram.Bot(os.getenv("BOT_TOKEN"))
-
-            # await bot.send_message(text='Hi John!', chat_id=1234567890)
-            chat_id = -1001943204451
-            current_state = tracker.current_state()
-            telegram_metadata = current_state['latest_message']['metadata']
-            username = telegram_metadata['username']
-            first_name = telegram_metadata['first_name']
-            name = f'{first_name} / {username}'
-
-            created_topic = bot.create_forum_topic(chat_id, name)
-            dbname = get_database()
-            collection_name = dbname["users1"]
-            new_user = {
-                "telegram_user_id": telegram_metadata['telegram_user_id'],
-                "username": username,
-                "first_name": first_name,
-                "topic_id": created_topic['message_thread_id'],
-                "admin_chat_id": chat_id,
-                "user_chat_id": telegram_metadata['telegram_user_id'],
-            }
-            # bot.send_message(chat_id, str(new_user), reply_to_message_id=created_topic['message_thread_id'])
-            events = current_state['events']
-            logger.info(events)
-            logger.info("event linst")
-            x = collection_name.insert_one(new_user)
-
-            logger.info(f"telegram answer {name}, {created_topic}, {x.inserted_id}, {x.acknowledged}")
-
-            for event in events:
-                print(event)
-                while True:
-                    try:
-                        if 'event' in event and event['event'] == 'user':
-                            bot.send_message(chat_id, event['text'],
-                                             reply_to_message_id=created_topic['message_thread_id'], timeout=30)
-                        elif 'event' in event and event['event'] == 'bot':
-                            bot.send_message(chat_id, f">>BOT\n{event['text']}",
-                                             reply_to_message_id=created_topic['message_thread_id'], timeout=30)
-                        sleep(1)
-                        break
-                    except (telegram.error.TimedOut, telegram.error.RetryAfter) as exc:
-                        logger.warning(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
-                        sleep(5)
-
-            while True:
-                try:
-                    bot.send_message(chat_id, '=== Конец истории сообщений ===',
-                                     reply_to_message_id=created_topic['message_thread_id'],
-                                     timeout=30)
-                    break
-                except (telegram.error.TimedOut, telegram.error.RetryAfter) as exc:
-                    logger.warning(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
-                    sleep(5)
-
-
-        except Exception as exc:
-            logger.error(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
+        # print("create_topic")
+        # try:
+        #     bot = telegram.Bot(os.getenv("BOT_TOKEN"))
+        #
+        #     # await bot.send_message(text='Hi John!', chat_id=1234567890)
+        #     chat_id = -1001943204451
+        #     current_state = tracker.current_state()
+        #     telegram_metadata = current_state['latest_message']['metadata']
+        #     username = telegram_metadata['username']
+        #     first_name = telegram_metadata['first_name']
+        #     name = f'{first_name} / {username}'
+        #
+        #     created_topic = bot.create_forum_topic(chat_id, name)
+        #     dbname = get_database()
+        #     collection_name = dbname["users1"]
+        #     new_user = {
+        #         "telegram_user_id": telegram_metadata['telegram_user_id'],
+        #         "username": username,
+        #         "first_name": first_name,
+        #         "topic_id": created_topic['message_thread_id'],
+        #         "admin_chat_id": chat_id,
+        #         "user_chat_id": telegram_metadata['telegram_user_id'],
+        #     }
+        #     # bot.send_message(chat_id, str(new_user), reply_to_message_id=created_topic['message_thread_id'])
+        #     events = current_state['events']
+        #     logger.info(events)
+        #     logger.info("event linst")
+        #     x = collection_name.insert_one(new_user)
+        #
+        #     logger.info(f"telegram answer {name}, {created_topic}, {x.inserted_id}, {x.acknowledged}")
+        #
+        #     for event in events:
+        #         print(event)
+        #         while True:
+        #             try:
+        #                 if 'event' in event and event['event'] == 'user':
+        #                     bot.send_message(chat_id, event['text'],
+        #                                      reply_to_message_id=created_topic['message_thread_id'], timeout=30)
+        #                 elif 'event' in event and event['event'] == 'bot':
+        #                     bot.send_message(chat_id, f">>BOT\n{event['text']}",
+        #                                      reply_to_message_id=created_topic['message_thread_id'], timeout=30)
+        #                 sleep(1)
+        #                 break
+        #             except (telegram.error.TimedOut, telegram.error.RetryAfter) as exc:
+        #                 logger.warning(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
+        #                 sleep(5)
+        #
+        #     while True:
+        #         try:
+        #             bot.send_message(chat_id, '=== Конец истории сообщений ===',
+        #                              reply_to_message_id=created_topic['message_thread_id'],
+        #                              timeout=30)
+        #             break
+        #         except (telegram.error.TimedOut, telegram.error.RetryAfter) as exc:
+        #             logger.warning(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
+        #             sleep(5)
+        #
+        #
+        # except Exception as exc:
+        #     logger.error(f"!!!!!!!!!!!!!! EXCEPTION: !!!!!!!!!!!!!!!!!!! {exc}, {traceback.format_exc()}")
 
         return []
